5545c3c4d78709a0750b32220b3776f6674aa609
Nahuel <nahuelastor@gmail.com>
2024-11-17 06:47:59 -0300

    traductor implementado al navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule, TranslateModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isEnglish: boolean = true;
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+    this.translate.use('es');
+  }
+
+  changeLanguage() {
+    console.log(this.isEnglish);
+    if (this.isEnglish) {
+      this.translate.use('es');
+    } else {
+      this.translate.use('en');
+    }
+  }
+
   isMenuOpen = false;
 
   toggleMenu() {
